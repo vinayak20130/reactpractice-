5a3ad63d9825ec76cd01ec34a11daed678313aa8
vinayak <vinayak20130@gmail.com>
2024-03-24 10:38:39 +0530

    456

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import Counter from './components/Counter-App/Counter';
 import Todolist from './components/To-Do-List/Todolist';
 import Colorchanger from './components/colorchanger/Colorchanger';
-
+import PasswordGenrator from './components/To-Do-List/PasswordGenrator/PasswordGenrator';
 
 function App() {
   return (
     <div>
-
-     <Colorchanger/>
+  <PasswordGenrator/>
+     {/* <Colorchanger/> */}
     </div>
   );
 }
